b0e4b1cda06a9df38cd7ad8473234f3f4dbddd04
Rasmus Wiuff <rwiuff@gmail.com>
2019-03-20 01:23:30 +0100

    Pfffft

--- Listings/Exercise7.py
+++ Listings/Exercise7.py
@@ -1,30 +1,38 @@
 import numpy as np
+import sympy as sym
 import math
-from sympy import *
+from sympy import I, pi, simplify, cos, sin, init_printing
 from matplotlib import pyplot as plt
+init_printing(use_unicode=True)
 
-
-h = Matrix([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
-V = Matrix([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
+h = sym.Matrix([[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]])
+V = sym.Matrix([[0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
 VT = V.T
-k = symbols('k')
-e = symbols('e')
-hVVT = h + V * exp(-I * k) + VT * exp(-I * k)
-bcp = hVVT - Matrix([[1, 0, 0, 0], [0, 1, 0, 0],
-                     [0, 0, 1, 0], [0, 0, 0, 1]]) * e
-cp = bcp.det()
-print(cp)
-# e = hVVT.eigenvals()
-# e = list(e.keys())
-# print(e[0].rewrite(exp).simplify())
-# for j in range(len(roots)):
-#     for k in range(np.size(r)):
-#         energy[0, i] = roots(k).evalf
+k = sym.symbols('k', real=True)
+lamda = sym.symbols('lamda', real=True)
+hVVT = h + V * sym.exp(-I * k) + VT * sym.exp(-I * k)
+m = hVVT - sym.eye(hVVT.shape[0]) * lamda
+char = sym.det(m)
+char = (char.as_real_imag())[0]
+char = sym.collect(simplify(char), k)
+print(char)
+eigenvals = sym.solve(char, lamda)
+print(eigenvals)
+
+# kpoint = np.linspace(-math.pi, math.pi, 100)
+# energy = np.zeros((len(eigenvals), 100))
+# f = np.array([])
+# for i in range(len(eigenvals)):
+#     f = np.append(f, sym.lambdify(k, eigenvals[i], "numpy"))
+#     for j in range(100):
+#         energy[i, j] = f[i](kpoint[j])
 #
-# plt.plot(k, energy[0], '-', label=r'$\epsilon_1$')
-# plt.plot(k, energy[1], '-', label=r'$\epsilon_2$')
-# plt.plot(k, energy[2], '-', label=r'$\epsilon_3$')
-# plt.plot(k, energy[3], '-', label=r'$\epsilon_4$')
+# # print(energy)
+#
+# plt.plot(kpoint, energy[0], '-', label=r'$\epsilon_1$')
+# plt.plot(kpoint, energy[1], '-', label=r'$\epsilon_2$')
+# plt.plot(kpoint, energy[2], '-', label=r'$\epsilon_3$')
+# plt.plot(kpoint, energy[3], '-', label=r'$\epsilon_4$')
 # plt.xlabel('k')
 # plt.ylabel('Energy')
 # plt.grid(True)
