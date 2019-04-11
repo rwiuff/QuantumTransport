db27bef975de49ed1736856623a0f5b160d30312
Rasmus Wiuff <rwiuff@gmail.com>
2019-04-11 14:16:46 +0200

    Complex stateplots sorta working

--- Listings/Development.py
+++ Listings/Development.py
@@ -1,46 +1,40 @@
 from matplotlib import pyplot as plt     # Pyplot for nice graphs
 from mpl_toolkits.mplot3d import Axes3D  # Used for 3D plots
 from matplotlib.widgets import Slider, Button
+import matplotlib
 import numpy as np                      # NumPy
+import seaborn
 from numpy import linalg as LA
 from collections import Counter
-from Functions import xyzimport, Hkay
+from Functions import xyzimport, Hkay, Onsite, Hop
+
+# Set hopping potential
 Vppi = -1
 
+# Define lattice vectors
 shiftx = 32.7862152500
 shifty = 8.6934634800
+
+# Retrieve unit cell
 xyz = xyzimport('fab_NPG_C.fdf')
-Ham = np.zeros((xyz.shape[0], xyz.shape[0]))
-for i in range(xyz.shape[0]):
-    for j in range(xyz.shape[0]):
-        Ham[i, j] = LA.norm(np.subtract(xyz[i], xyz[j]))
-Ham = np.where(Ham < 1.6, Vppi, 0)
-Ham = np.subtract(Ham, Vppi * np.identity(xyz.shape[0]))
+# Calculate onsite nearest neighbours
+Ham = Onsite(xyz, Vppi)
 
+# Shift unit cell
 xyz1 = xyz + np.array([shiftx, 0, 0])
-V1 = np.zeros((xyz.shape[0], xyz.shape[0]))
+# Calculate offsite nearest neighbours
+V1 = Hop(xyz, xyz1, Vppi)
 
-for i in range(xyz.shape[0]):
-    for j in range(xyz1.shape[0]):
-        V1[i, j] = LA.norm(np.subtract(xyz[i], xyz1[j]))
-V1 = np.where(V1 < 1.6, Vppi, 0)
-
+# Shift unit cell
 xyz2 = xyz + np.array([0, shifty, 0])
-V2 = np.zeros((xyz.shape[0], xyz.shape[0]))
+# Calculate offsite nearest neighbours
+V2 = Hop(xyz, xyz2, Vppi)
 
-for i in range(xyz.shape[0]):
-    for j in range(xyz2.shape[0]):
-        V2[i, j] = LA.norm(np.subtract(xyz[i], xyz2[j]))
-V2 = np.where(V2 < 1.6, Vppi, 0)
-
-
+# Shift unit cell
 xyz3 = xyz + np.array([shiftx, shifty, 0])
-V3 = np.zeros((xyz.shape[0], xyz.shape[0]))
+# Calculate offsite nearest neighbours
+V3 = Hop(xyz, xyz3, Vppi)
 
-for i in range(xyz.shape[0]):
-    for j in range(xyz3.shape[0]):
-        V3[i, j] = LA.norm(np.subtract(xyz[i], xyz3[j]))
-V3 = np.where(V3 < 1.6, Vppi, 0)
 
 print(np.sum(Ham))
 # plt.imshow(Ham)
@@ -56,51 +50,58 @@ print(np.sum(Ham))
 # plt.colorbar()
 # plt.show()
 
-
+# Define k-space range
 k = np.linspace(0, np.pi, 1000)
+# Array for X-bands
 X = np.zeros((Ham.shape[0], k.size))
+# Array for Z-bands
 Z = np.zeros((Ham.shape[0], k.size))
+# Get bands from gamma to X and Z
 for i in range(k.shape[0]):
     X[:, i] = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=-k[i], y=0)[0]
     Z[:, i] = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=0, y=k[i])[0]
+# Get energies at k(0,0)
 zero = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=0, y=0)[0]
+# Renormalise distances according to lattice vectors
 Xspace = np.linspace(0, 1 / shifty, 1000)
 Zspace = np.linspace(0, 1 / shiftx, 1000)
+# Plot Bandstructures
 ax = plt.figure(figsize=(1, 6))
 for i in range(X.shape[0]):
-    plt.plot(np.flip(-Zspace, axis=0), np.flip(X[i, :], axis=0))
-for i in range(X.shape[0]):
-    plt.plot(Xspace, Z[i, :])
+    plt.plot(np.flip(-Zspace, axis=0),
+             np.flip(X[i, :], axis=0), 'k', linewidth=1)
+    plt.plot(Xspace, Z[i, :], 'k', linewidth=1)
 xtick = np.array([-1 / shiftx, 0, 1 / shifty])
-plt.xticks(xtick, ('X', 'G', 'Z'))
+plt.xticks(xtick, ('X', r'$\Gamma$', 'Z'))
 plt.axvline(x=0, linewidth=1, color='k', linestyle='--')
 plt.title('NPG-normal')
 plt.ylim(-1, 1)
 plt.show()
 
-
+# Get eigenvalues/vectors for degeneracy and stateplot
 e, v = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=0, y=0)
 e = np.round(e, decimals=3)
 w = e.real
 c = Counter(w)
 y = np.array([p for k, p in sorted(c.items())])
 x = np.asarray(sorted([*c]))
-# fig, ax = plt.subplots(figsize=(16, 10), dpi=80)
-# ax.vlines(x=x, ymin=0, ymax=y,
-#           color='firebrick', alpha=0.7, linewidth=2)
-# ax.scatter(x=x, y=y, s=75, color='firebrick', alpha=0.7)
-#
-# ax.set_title('Energy degeneracy', fontdict={'size': 22})
-# ax.set_ylabel('Degeneracy')
-# ax.set_xlabel('Energy')
-# ax.set_ylim(0, 10)
-# ax.tick_params(axis='both', which='both')
-# ax.spines['left'].set_position('center')
-# plt.grid(which='both')
-# for i in range(x.size):
-#     ax.text(x[i], y[i] + .5, s=x[i], horizontalalignment='center',
-#             verticalalignment='bottom', fontsize=14)
-# plt.show()
+fig, ax = plt.subplots(figsize=(16, 10), dpi=80)
+ax.vlines(x=x, ymin=0, ymax=y,
+          color='firebrick', alpha=0.7, linewidth=2)
+ax.scatter(x=x, y=y, s=75, color='firebrick', alpha=0.7)
+
+ax.set_title('Energy degeneracy', fontdict={'size': 22})
+ax.set_ylabel('Degeneracy')
+ax.set_xlabel('Energy')
+ax.set_ylim(0, 10)
+ax.tick_params(axis='both', which='both')
+ax.spines['left'].set_position('center')
+plt.grid(which='both')
+for i in range(x.size):
+    ax.text(x[i], y[i] + .5, s=x[i], horizontalalignment='center',
+            verticalalignment='bottom', fontsize=14)
+seaborn.despine(left=True, bottom=True, right=True)
+plt.show()
 
 xlin = np.array([[0, 0]])
 ylin = np.array([[0, 0]])
@@ -123,7 +124,21 @@ for i in range(xlin.shape[0]):
 
 ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2], zdir='z', s=300)
 plt.gca().set_aspect('equal', adjustable='box')
+ax.set_zlim(0, 50)
+ax.set_xlabel('X: [Å]')
+ax.set_ylabel('Y: [Å]')
+ax.set_zlabel('Z: [Å]')
 plt.show()
+
+fig = plt.figure()
+for i in range(xlin.shape[0]):
+    plt.plot(xlin[i], ylin[i])
+plt.scatter(xyz[:, 0], xyz[:, 1], s=300)
+plt.gca().set_aspect('equal', adjustable='box')
+plt.ylabel('[Å]')
+plt.xlabel('[Å]')
+plt.show()
+
 
 val = 1
 fig = plt.figure()
@@ -168,4 +183,147 @@ def reset(event):
 button.on_clicked(reset)
 state.on_changed(update)
 plt.gca().set_aspect('equal', adjustable='box')
+ax.set_zlim(0, 50)
+ax.set_xlabel('X: [Å]')
+ax.set_ylabel('Y: [Å]')
+ax.set_zlabel('Z: [Å]')
+plt.show()
+
+
+e, v = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=np.pi, y=0)
+val = 1
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i in range(xlin.shape[0]):
+    ax.plot(xlin[i], ylin[i], zlin[i])
+s = np.zeros(v.shape[0])
+c = np.zeros(v.shape[0])
+colors = np.zeros((v.shape[0], 4))
+val = 1
+s = np.absolute(v[:, val - 1])
+s = s * 900
+cmap = matplotlib.cm.get_cmap('hsv')
+c[:] = np.angle(v[:, val-1])
+vmin = np.min(c)
+vmax = np.max(c)
+normalize = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+# print(np.angle(v[:, val - 1]))
+# print(normalize(np.angle(v[:, val - 1])))
+# print(cmap(normalize(np.angle(v[:, val - 1]))))
+
+colors[:] = cmap(normalize(np.angle(v[:, val - 1])))
+print(colors)
+# c = np.where(v[:, val - 1] > 0, 0, 1)
+Stateplot = ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2], zdir='z', s=s)
+plt.subplots_adjust(bottom=0.25)
+axcolor = 'lightgoldenrodyellow'
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+state = Slider(axfreq, 'State', 1, v.shape[0], valinit=1, valstep=1)
+
+
+def update(val):
+    val = state.val
+    val = int(val)
+    s = np.absolute(v[:, val - 1])
+    s = s * 900
+    print(s)
+    colors = np.zeros((v.shape[0], 4))
+    c[:] = np.angle(v[:, val-1])
+    vmin = np.min(c)
+    vmax = np.max(c)
+    normalize = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+    colors[:] = cmap(normalize(np.angle(v[:, val - 1])))
+    print(colors)
+    # c = np.where(v[:, val - 1] > 0, 0, 1)
+    Stateplot._sizes = s
+    Stateplot.set_array(c)
+    fig.canvas.draw_idle()
+
+
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', hovercolor='0.975')
+
+
+def reset(event):
+    state.reset()
+
+
+button.on_clicked(reset)
+state.on_changed(update)
+plt.gca().set_aspect('equal', adjustable='box')
+ax.set_zlim(0, 50)
+ax.set_xlabel('X: [Å]')
+ax.set_ylabel('Y: [Å]')
+ax.set_zlabel('Z: [Å]')
+cax, _ = matplotlib.colorbar.make_axes(ax)
+cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize)
+plt.show()
+
+e, v = Hkay(Ham=Ham, V1=V1, V2=V2, V3=V3, x=0, y=np.pi)
+val = 1
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i in range(xlin.shape[0]):
+    ax.plot(xlin[i], ylin[i], zlin[i])
+s = np.zeros(v.shape[0])
+c = np.zeros(v.shape[0])
+colors = np.zeros((v.shape[0], 4))
+val = 1
+s = np.absolute(v[:, val - 1])
+s = s * 900
+cmap = matplotlib.cm.get_cmap('hsv')
+c[:] = np.angle(v[:, val-1])
+vmin = np.min(c)
+vmax = np.max(c)
+normalize = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+# print(np.angle(v[:, val - 1]))
+# print(normalize(np.angle(v[:, val - 1])))
+# print(cmap(normalize(np.angle(v[:, val - 1]))))
+
+colors[:] = cmap(normalize(np.angle(v[:, val - 1])))
+print(colors)
+# c = np.where(v[:, val - 1] > 0, 0, 1)
+Stateplot = ax.scatter(xyz[:, 0], xyz[:, 1], xyz[:, 2], zdir='z', s=s)
+plt.subplots_adjust(bottom=0.25)
+axcolor = 'lightgoldenrodyellow'
+axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+state = Slider(axfreq, 'State', 1, v.shape[0], valinit=1, valstep=1)
+
+
+def update(val):
+    val = state.val
+    val = int(val)
+    s = np.absolute(v[:, val - 1])
+    s = s * 900
+    print(s)
+    colors = np.zeros((v.shape[0], 4))
+    c[:] = np.angle(v[:, val-1])
+    vmin = np.min(c)
+    vmax = np.max(c)
+    normalize = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+    colors[:] = cmap(normalize(np.angle(v[:, val - 1])))
+    print(colors)
+    # c = np.where(v[:, val - 1] > 0, 0, 1)
+    Stateplot._sizes = s
+    Stateplot.set_array(c)
+    fig.canvas.draw_idle()
+
+
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button = Button(resetax, 'Reset', hovercolor='0.975')
+
+
+def reset(event):
+    state.reset()
+
+
+button.on_clicked(reset)
+state.on_changed(update)
+plt.gca().set_aspect('equal', adjustable='box')
+ax.set_zlim(0, 50)
+ax.set_xlabel('X: [Å]')
+ax.set_ylabel('Y: [Å]')
+ax.set_zlabel('Z: [Å]')
+cax, _ = matplotlib.colorbar.make_axes(ax)
+cbar = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=normalize)
 plt.show()
--- Listings/Functions.py
+++ Listings/Functions.py
@@ -15,6 +15,25 @@ def xyzimport(path):
     return xyz
 
 
+def Onsite(xyz, Vppi):
+    h = np.zeros((xyz.shape[0], xyz.shape[0]))
+    for i in range(xyz.shape[0]):
+        for j in range(xyz.shape[0]):
+            h[i, j] = LA.norm(np.subtract(xyz[i], xyz[j]))
+    h = np.where(h < 1.6, Vppi, 0)
+    h = np.subtract(h, Vppi * np.identity(xyz.shape[0]))
+    return h
+
+
+def Hop(xyz, xyz1, Vppi):
+    hop = np.zeros((xyz.shape[0], xyz.shape[0]))
+    for i in range(xyz.shape[0]):
+        for j in range(xyz1.shape[0]):
+            hop[i, j] = LA.norm(np.subtract(xyz[i], xyz1[j]))
+    hop = np.where(hop < 1.6, Vppi, 0)
+    return hop
+
+
 def Hkay(Ham, V1, V2, V3, x, y):
     Ham = Ham + (V1 * np.exp(-1.0j * x)
                  + np.transpose(V1) * np.exp(1.0j * x)
