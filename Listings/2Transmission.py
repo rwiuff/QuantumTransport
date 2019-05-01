91654315ed9c0ebf512c0bd0316264e4f6bc4612
Rasmus Wiuff <rwiuff@gmail.com>
2019-05-01 11:20:48 +0200

    Transmission almost working

--- Listings/2Transmission.py
+++ Listings/2Transmission.py
@@ -84,69 +84,53 @@ for i in range(xyz[:, 0].shape[0]):
     plt.annotate(s, xy)
 plt.show()
 
-RmArray = np.append(Lcell, Rcell).astype(int)
-
-Cxyz = np.delete(xyz, RmArray, 0)
-
-Hc = Onsite(xyz=Cxyz, Vppi=-1)
-plt.imshow(Hc)
-plt.colorbar()
-plt.show()
 HD = Onsite(xyz=xyz, Vppi=-1)
-plt.imshow(HD)
-plt.colorbar()
-plt.show()
 HL = Onsite(xyz=Lxyz, Vppi=-1)
-plt.imshow(HL)
-plt.colorbar()
-plt.show()
 HR = Onsite(xyz=Rxyz, Vppi=-1)
-plt.imshow(HR)
-plt.colorbar()
-plt.show()
 shiftx = 2.46
 Lxyz1 = Lxyz - np.array([shiftx, 0, 0])
 Rxyz1 = Rxyz + np.array([shiftx, 0, 0])
 VL = Hop(xyz=Lxyz, xyz1=Lxyz1, Vppi=-1)
-plt.imshow(VL)
-plt.colorbar()
-plt.show()
-VLDak = Hop(xyz=xyz, xyz1=Lxyz1, Vppi=-1)
-plt.imshow(VLDak)
-plt.colorbar()
-plt.show()
-VRDak = Hop(xyz=Rxyz, xyz1=Rxyz1, Vppi=-1)
-plt.imshow(VRDak)
-plt.colorbar()
-plt.show()
-VR = Hop(xyz=xyz, xyz1=Rxyz1, Vppi=-1)
-plt.imshow(VR)
-plt.colorbar()
-plt.show()
+VR = Hop(xyz=Rxyz, xyz1=Rxyz1, Vppi=-1)
+# plt.imshow(HD)
+# plt.colorbar()
+# plt.show()
+# plt.imshow(HL)
+# plt.colorbar()
+# plt.show()
+# plt.imshow(HR)
+# plt.colorbar()
+# plt.show()
+# plt.imshow(VL)
+# plt.colorbar()
+# plt.show()
+# plt.imshow(VR)
+# plt.colorbar()
+# plt.show()
+
 En = np.linspace(-3, 3, 1000)
-GD = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]])
-GammaL = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]])
-GammaR = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]])
+GD = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]], dtype=complex)
+GammaL = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]], dtype=complex)
+GammaR = np.zeros([En.shape[0], HD.shape[0], HD.shape[1]], dtype=complex)
 q = 0
+eta = 1e-6j
 for i in En:
-    gl, SelfER, SelfEL = RecursionRoutine(i, HL, VL)
-    gr, SelfER, SelfEL = RecursionRoutine(i, HR, VRDak)
-    print(gl.shape)
-    print(gr.shape)
-    SelfEL = VL @ gl @ VLDak
-    print(VRDak.shape)
-    print(VR.shape)
-    SelfER = VRDak @ gl @ VR
-    print(SelfEL.shape)
-    print(SelfER.shape)
-    Diff = HD.shape[0] - SelfEL.shape[0]
-    Blowup = np.zeros((Diff, HD.shape[1]))
-    SelfEL, SelfER = np.append(
-        SelfEL, Blowup, axis=0), np.append(SelfER, Blowup, axis=0)
+    gl, crap, SEL = RecursionRoutine(i, HL, VL, eta=eta)
+    gr, SER, crap = RecursionRoutine(i, HR, VR, eta=eta)
+    SS = SEL.shape[0]
+
+    Matrix = np.zeros((HD.shape), dtype=complex)
+    Matrix[0:SS, 0:SS] = SEL
+    SEL = Matrix
+
+    Matrix = np.zeros((HD.shape), dtype=complex)
+    Matrix[-SS:, -SS:] = SER
+    SER = Matrix
+
     GD[q] = LA.inv(np.identity(HD.shape[0]) *
-                   (i - 1e-6j) - HD - SelfEL - SelfER)
-    GammaL[q] = 1j * (SelfEL - np.transpose(SelfEL))
-    GammaR[q] = 1j * (SelfER - np.transpose(SelfER))
+                   (i + eta) - HD - SEL - SER)
+    GammaL[q] = 1j * (SEL - SEL.conj().T)
+    GammaR[q] = 1j * (SER - SER.conj().T)
     q = q + 1
 
 G00 = np.zeros((En.shape[0]), dtype=complex)
@@ -160,11 +144,11 @@ Y1 = Y.real
 Y2 = Y.imag
 # Y1 = np.sort(Y1)
 # Y2 = np.sort(Y2)
-# print(Y1, Y2)
+print(Y1, Y2)
 real, = plt.plot(X, Y1, label='real')
 # imag, = plt.plot(X, Y2, label='imag')
 imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-plt.ylim((-20, 40))
+plt.ylim((-10, 20))
 # plt.axis('equal')
 plt.grid(which='major', axis='both')
 plt.legend(handles=[imag, real])
@@ -172,12 +156,12 @@ plt.show()
 plt.title('Greens function of a simple four-atom unit cell')
 plt.xlabel('Energy E arb. unit')
 plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.savefig('imrealplot.eps', bbox_inches='tight')
+plt.savefig('imrealTE.eps', bbox_inches='tight')
 
-T = np.zeros(En.shape[0])
+T = np.zeros(En.shape[0], dtype=complex)
 for i in range(En.shape[0]):
-    T[i] = np.trace(GammaR[i] @ GD[i] @ GammaL[i] @ np.transpose(GD[i]))
-
+    T[i] = np.trace(GammaR[i] @ GD[i] @ GammaL[i] @ GD[i].conj().T)
+print(T)
 Y = T
 X = En
 plt.plot(X, Y)
