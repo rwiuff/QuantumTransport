5911b25af046a2bb79043e8e4785abd0df522055
Rasmus Wiuff <rwiuff@gmail.com>
2019-05-20 13:34:34 +0200

    For loop creates as many subplots as needed

--- Listings/NPGTransmission.py
+++ Listings/NPGTransmission.py
@@ -12,12 +12,15 @@ nx = 1
 ny = 2
 contactrep = 1
 shiftx = 2.46
-En = np.linspace(-3, 3, 200)
+ev1 = -1.5
+ev2 = 1.5
+numkP = 5
+En = np.linspace(ev1 / 2.7, ev2 / 2.7, 200)
 En = np.delete(En, np.where(En == 1))
 En = np.delete(En, np.where(En == -1))
 
 eta = 1e-6j
-kP = np.linspace(-np.pi, np.pi, 5)
+kP = np.linspace(-np.pi, np.pi, numkP)
 
 xyz, UX, UY, filename, dgeom, cellsize = Import(nx, contactrep)
 
@@ -27,7 +30,8 @@ TT = np.zeros((kP.shape[0], En.shape[0]))
 GG = np.zeros((kP.shape[0], En.shape[0]), dtype=complex)
 q = 0
 for i in kP:
-    print('Calculating for k-point: {}'.format(i))
+    print('----------------------------------------------------------------------')
+    print('Calculating for k-point:    {}'.format(i))
     Ham = PeriodicHamiltonian(xyz, UY, i)
     HL = Ham[L]
     HL = HL[:, L]
@@ -37,22 +41,22 @@ for i in kP:
     VL = VL[:, RestL]
     VR = Ham[RestR]
     VR = VR[:, R]
-    gs = GridSpec(2, 2, width_ratios=[1, 2])
-    a = plt.figure(figsize=(7, 4))
-    ax1 = plt.subplot(gs[:, 1])
-    plt.imshow(Ham.real)
-    ax2 = plt.subplot(gs[0, 0])
-    plt.imshow(HL.real)
-    ax3 = plt.subplot(gs[1, 0])
-    plt.imshow(HR.real)
-    a.show()
-    b = plt.figure(figsize=(7, 4))
-    plt.subplot(121)
-    plt.imshow(VL.real)
-    plt.subplot(122)
-    plt.imshow(VR.real)
-    b.show()
-    input('Press any key to continue')
+    # gs = GridSpec(2, 2, width_ratios=[1, 2])
+    # a = plt.figure(figsize=(7, 4))
+    # ax1 = plt.subplot(gs[:, 1])
+    # plt.imshow(Ham.real)
+    # ax2 = plt.subplot(gs[0, 0])
+    # plt.imshow(HL.real)
+    # ax3 = plt.subplot(gs[1, 0])
+    # plt.imshow(HR.real)
+    # a.show()
+    # b = plt.figure(figsize=(7, 4))
+    # plt.subplot(121)
+    # plt.imshow(VL.real)
+    # plt.subplot(122)
+    # plt.imshow(VR.real)
+    # b.show()
+    # input('Press any key to continue')
 
     GD, GammaL, GammaR = EnergyRecursion(Ham, HL, HR, VL, VR, En, eta)
 
@@ -68,140 +72,80 @@ for i in kP:
     GG[q, :] = G
     TT[q, :] = T.real
     q = q + 1
+X = En * 2.7
+Y0 = 0
+numplot = numkP + 1
+if numplot % 2 == 0:
+    if numplot % 3 == 0:
+        ncol = 3
+    else:
+        ncol = 2
+else:
+    if numplot % 3 == 0:
+        ncol = 3
+nrow = numplot / ncol
+numplot = int(numplot)
+nrow = int(nrow)
+ncol = int(ncol)
+q = int(0)
+numplot = int(numplot)
 print('Plotting Greens functions')
-plt.subplot(231)
-Y = GG[0, :]
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.subplot(232)
-Y = GG[1, :]
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.subplot(233)
-Y = GG[2, :]
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.subplot(234)
-Y = GG[3, :]
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.subplot(235)
-Y = GG[4, :]
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.subplot(236)
-G = np.average(GG, axis=0)
-Y = G
-X = En
-Y1 = Y.real
-Y2 = Y.imag
-real, = plt.plot(X, Y1, label='real')
-imag, = plt.fill(X, Y2, c='orange', alpha=0.8, label='imag')
-# plt.ylim((-10, 20))
-plt.grid(which='both', axis='both')
-plt.legend(handles=[imag, real])
-plt.xlabel('Energy E arb. unit')
-plt.ylabel('Re[G00(E)]/Im[G00(E)]')
-plt.title('Greens function at 0th site')
-# savename = filename.replace('.fdf', 'AverageimrealTE.eps')
-# plt.savefig(savename, bbox_inches='tight')
+for i in range(nrow):
+    for j in range(ncol):
+        if q + 1 == numplot:
+            subplotn = "{}{}{}".format(nrow, ncol, numplot)
+            plt.subplot(int(subplotn))
+            G = np.average(GG, axis=0)
+            Y = G
+            Y1 = Y.real
+            Y2 = Y.imag
+            real, = plt.plot(X, Y1, label='real')
+            imag, = plt.fill(X, Y2, color='orange', alpha=0.8, label='imag')
+            plt.grid(which='both', axis='both')
+            plt.legend(handles=[imag, real])
+            plt.xlabel('Energy E arb. unit')
+            plt.ylabel('Re[G00(E)]/Im[G00(E)]')
+            plt.title('Greens function at 0th site')
+        else:
+            subplotn = "{}{}{}".format(nrow, ncol, int(q + 1))
+            plt.subplot(int(subplotn))
+            Y = GG[q, :]
+            Y1 = Y.real
+            Y2 = Y.imag
+            real, = plt.plot(X, Y1, label='real')
+            imag, = plt.fill(X, Y2, color='orange', alpha=0.8, label='imag')
+            plt.grid(which='both', axis='both')
+            plt.legend(handles=[imag, real])
+            plt.xlabel('Energy E arb. unit')
+            plt.ylabel('Re[G00(E)]/Im[G00(E)]')
+            q = q + int(1)
 plt.show()
+
+q = int(0)
 print('Plotting Transmission')
-plt.subplot(231)
-T = TT[0]
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.subplot(232)
-T = TT[1]
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.subplot(233)
-T = TT[2]
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.subplot(234)
-T = TT[3]
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.subplot(235)
-T = TT[4]
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.subplot(236)
-T = np.average(TT, axis=0)
-Y = T.real
-X = En
-plt.plot(X, Y)
-# plt.ylim((0, 1))
-plt.grid(which='both', axis='both')
-plt.xlabel(r'$E(V_{pp\pi})$')
-plt.ylabel(r'T(E)')
-plt.title('Average Transmission')
-# savename = filename.replace('.fdf', 'AverageTE.eps')
-# plt.savefig(savename, bbox_inches='tight')
+for i in range(nrow):
+    for j in range(ncol):
+        if q + 1 == numplot:
+            subplotn = "{}{}{}".format(nrow, ncol, numplot)
+            plt.subplot(int(subplotn))
+            T = np.average(TT, axis=0)
+            Y = T.real
+            plt.plot(X, Y)
+            plt.grid(which='both', axis='both')
+            plt.xlabel(r'$E(V_{pp\pi})$')
+            plt.ylabel(r'T(E)')
+            plt.title('Average Transmission')
+        else:
+            subplotn = "{}{}{}".format(nrow, ncol, int(q + 1))
+            plt.subplot(int(subplotn))
+            T = TT[q]
+            Y = T.real
+            plt.plot(X, Y)
+            plt.grid(which='both', axis='both')
+            plt.xlabel(r'$E(V_{pp\pi})$')
+            plt.ylabel(r'T(E)')
+            q = q + int(1)
 plt.show()
+
+input("Press any key to quit")
+quit()
